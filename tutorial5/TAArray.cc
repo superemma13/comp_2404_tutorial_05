15f48eedae0351a44eb79f1e9661874718b4e6f4
Emily Amos <emma13.amos@gmail.com>
2025-02-14 23:15:12 -0500

    fixed errors

--- tutorial5/TAArray.cc
+++ tutorial5/TAArray.cc
@@ -14,11 +14,11 @@ TAArray::~TAArray(){
     delete [] elements;
 }
 
-bool TextArea::add(TextArea* ta) {
+bool TAArray::add(TextArea* ta) {
     if (size >= MAX_COMPONENTS)return false;
 
-    for (int i = size; i > 0 ; --i){
-        if (ta->lessThan(*elements[i-1])){
+    /*for (int i = size; i > 0 ; --i){
+        if (ta->getID() < elements[i-1]->getID()){
             //keep making space by copying elements
             //one location to the right
             elements[i]=elements[i-1];
@@ -32,14 +32,30 @@ bool TextArea::add(TextArea* ta) {
 
     //we went through every location, and s was less than 
     //all of them. Therefore s goes in location 0
-    elements[0] = s;
+    elements[0] = ta;*/
+
+    elements[size] = ta;
+
     ++size;
     return true;
 }
 
-bool TextArea::get(const string& id, TextArea** ta) {
+bool TAArray::add(TextArea* ta, int index) {
+    if (size >= MAX_COMPONENTS)return false;
+    if (index < 0 || index > size)return false;
+
+    for (int i = size; i > index; --i) {
+        elements[i] = elements[i-1];
+    }
+
+    elements[index] = ta;
+    ++size;
+    return true;
+}
+
+bool TAArray::get(const string& id, TextArea** ta) {
     for (int i = 0; i < size; ++i){
-        if (name == elements[i]->getID()){
+        if (id == elements[i]->getID()){
             *ta = elements[i];
             return true;
         }
@@ -47,13 +63,27 @@ bool TextArea::get(const string& id, TextArea** ta) {
     return false;
 }
 
-bool TextArea::get(int index, TextArea** ta) {
+bool TAArray::get(int index, TextArea** ta) const {
     if (index < 0 || index >= size) return false;
     *ta = elements[index];
     return true;
 }
 
-bool remove(const string& id, TextArea** ta) {
+TextArea* TAArray::get(int index) const {
+    if (index < 0 || index >= size) return nullptr;
+    return elements[index];
+}
+
+TextArea* TAArray::get(const string& id) const {
+    for (int i = 0; i < size; ++i){
+        if (id == elements[i]->getID()){
+            return elements[i];
+        }
+    }
+    return nullptr;
+}
+
+bool TAArray::remove(const string& id, TextArea** ta) {
     int index = 0;
     //we want to find the index of the element to remove
     while (id != elements[index]->getID() && index < size){
@@ -76,7 +106,46 @@ bool remove(const string& id, TextArea** ta) {
     return true;
 }
 
-void removeUpTo(const string& id, List& goners) {
+TextArea* TAArray::remove(int index) {
+    if (index < 0 || index >= size) return nullptr;
+    
+    TextArea* ta = elements[index];
+
+    for (int i = index; i < size - 1; ++i) {
+        elements[i] = elements[i + 1];
+    }
+    elements[size - 1] = nullptr;
+    --size;
+    return ta;
+}
+
+TextArea* TAArray::remove(const string& id) {
+    int index = 0;
+
+    if (size == 0) return nullptr;
+    //we want to find the index of the element to remove
+    while (index < size && id != elements[index]->getID()) {
+        ++index;
+    }
+
+    //did we fall off the end of the List?
+    if (index == size) return nullptr;
+
+    //we have found what we wanted to remove,
+    //assign the output value
+    TextArea* ta = elements[index];
+
+    //copy everything after index left one position
+    for (int i = index; i < size - 1; ++i) {
+        elements[i] = elements[i + 1];
+    }
+
+    elements[size - 1] = nullptr;
+    --size;
+    return ta;
+}
+
+void TAArray::removeUpTo(const string& id, TAArray& arr) {
     //find index of the first Student who comes after name
     int index = 0;
 
@@ -95,7 +164,7 @@ void removeUpTo(const string& id, List& goners) {
     size -= toRemove;
 }
 
-void print() {
+void TAArray::print() {
   cout << endl << endl << "Students:" << endl;
   for (int i=0; i<size; ++i)
     elements[i]->print();
--- tutorial5/TAArray.h
+++ tutorial5/TAArray.h
@@ -1,6 +1,8 @@
 #ifndef TAARRAY_H
 #define TAARRAY_H
 
+using namespace std;
+
 #include "defs.h"
 #include "TextArea.h"
 
@@ -13,10 +15,15 @@ class TAArray {
         
         // member functions
         bool add(TextArea*);
+        bool add(TextArea*, int index);
         bool get(const string& id, TextArea**);
-        bool get(int index, TextArea**);
+        bool get(int index, TextArea**) const;
+        TextArea* get(int index) const;
+        TextArea* get(const string& id) const;
         bool remove(const string& id, TextArea**);
-        void removeUpTo(const string& id, List& goners);
+        TextArea* remove(int index);
+        TextArea* remove(const string& id);
+        void removeUpTo(const string& id, TAArray& goners);
         inline int getSize(){ return size; }
         void print();
 
